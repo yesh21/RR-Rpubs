8b9277adb8ed25d2ca65175c85f1bb793b0a42e4
yaswanth Pulavarthi <sc19yp@leeds.ac.uk>
2020-09-19 09:46:15 +0100

    -- final

--- Shiny/mtcars/ui.R
+++ Shiny/mtcars/ui.R
@@ -1,28 +1,41 @@
 library(shiny)
 shinyUI(fluidPage(
-    titlePanel("Tabs!"),
+    titlePanel("Iris Data"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("SepalWidth", "Set Sepal Width",min = 2.0, max = 4.4,value = 2.5, step = 0.1),
-            sliderInput("PetalLength", "Set Petal Length",min = 1.0, max = 6.9,value = 2.5, step = 0.1),
-            sliderInput("PetalWidth", "Set Petal Width",min = 0.1, max = 2.5,value = 2.5, step = 0.1),
-            numericInput("Species", "Species?", 
-                         value = 1, min = 1, max = 3, step = 1),
-            
+            sliderInput("SepalWidth", "Set Sepal Width",min = 2.0, max = 4.4,value = 3.4, step = 0.1),
+            sliderInput("PetalLength", "Set Petal Length",min = 1.0, max = 6.9,value = 1.5, step = 0.1),
+            sliderInput("PetalWidth", "Set Petal Width",min = 0.1, max = 2.5,value = 0.2, step = 0.1),
+            selectInput("Species", "Species?",
+                        c("setosa" = "setosa",
+                          "versicolor" = "versicolor",
+                          "virginica" = "virginica")),
+             
             submitButton("Submit")
         ),
         mainPanel(
+            
             tabsetPanel(type = "tabs", 
                         tabPanel("Sepal Width", br(), textOutput("out1"),plotOutput("plot1"),
-                                 h3("Predicted Sepal Width:"),
-                                 textOutput("pred1"), textOutput("finalpred")), 
+                                 h3("Predicted Sepal Length wrt Sepal Width:"),
+                                 textOutput("pred1")), 
                         tabPanel("Petal Width", br(), textOutput("out2"),plotOutput("plot2"),
-                                 h3("Predicted Sepal Width:"),
+                                 h3("Predicted Sepal Lenght wrt Petal Length:"),
                                  textOutput("pred2")), 
-                        tabPanel("Petal L", br(), textOutput("out3"),plotOutput("plot3"),
-                                 h3("Predicted Sepal Width: "),
+                        tabPanel("Petal Length", br(), textOutput("out3"),plotOutput("plot3"),
+                                 h3("Predicted Sepal Length wrt Petal Width: "),
                                  textOutput("pred3"))
-            )
+            ),
+            h2("Best fit:"),
+            h5("(Predicted Sepal Length wrt Sepal.Width+Petal.Length+Petal.Width+Species)"),
+            verbatimTextOutput("finalpred"),
+            tags$head(tags$style(HTML("
+                            #finalpred {
+                              font-size: 25px;
+                            }
+                            ")))
+            
+            
         )
     )
 ))
