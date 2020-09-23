cd765487d26f0b0d6ad166d12de45fd217685be9
yaswanth Pulavarthi <sc19yp@leeds.ac.uk>
2020-09-23 15:28:08 +0100

    gradient colors

--- Shiftkey Shiny/WordPred/ui.R
+++ Shiftkey Shiny/WordPred/ui.R
@@ -1,24 +1,32 @@
 library(shiny)
 library(dplyr)
 library(stringr)
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = "bootstrap.css",
+# Define UI for application that runs a word predictor
+shinyUI(fluidPage(style = "background-color: #eec0c6;
+background-image: linear-gradient(315deg, #eec0c6 0%, #7ee8fa 74%);
+ background-size: cover;",
+
                   
-    
-
-
+        
     titlePanel(    h1("WORD PREDICTOR", align = "center")
 ),
 
 
 
 
-    fluidRow(
+    fluidRow(style = "background-color: #eec0c6;
+background-image: linear-gradient(315deg, #eec0c6 0%, #7ee8fa 74%);
+ background-size: cover;",
         
                       textInput("inputString", "Input text:", value = "have a"), width = 15, align = "center",
                       h4(HTML("<center>Predicted Next Word</center>")),
                       uiOutput("show")
                   )
+
+
+
+
+
     )
 )
 
